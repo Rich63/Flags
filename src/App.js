32ece2236a32ed8ba18280e48c71c8b9a92c8277
Richard Pos <richardnottoknow@gmail.com>
2019-05-07 09:00:15 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <header
           className="title-header"
           style={{ backgroundImage: `url(${worldImg})` }}>
-          <h1 className="title-text">Guess The Flag</h1>
+          <h1 className="title-text">Guess The Flags</h1>
         </header>
         <CountryGame />
       </div>
